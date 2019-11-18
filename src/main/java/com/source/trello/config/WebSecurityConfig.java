3c433ea85a5ca315584d6764dd362aa7a6763fb2
HoangTungSon <52152855+HoangTungSon@users.noreply.github.com>
2019-11-18 09:58:24 +0700

    commit

--- src/main/java/com/source/trello/config/WebSecurityConfig.java
+++ src/main/java/com/source/trello/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/", "/login").permitAll()
                 .and()
                 .authorizeRequests().antMatchers("/users/**").hasRole("USER")
                 .anyRequest().authenticated()
